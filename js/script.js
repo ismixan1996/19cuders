16dbdea2530572610ea1ee7faec29063b3cdcd27
ismixan <ismixan1996@mail.ru>
2022-08-18 19:30:16 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+ let button= document.getElementsByClassName('buttonSelf') [0];
+
+
+let container=document.getElementById('container')
+button.addEventListener('click',function() {
+    let img=document.createElement('img')
+
+
+img.src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'
+    container.appendChild(img)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let ulElement=document.querySelector('ul')
+
+
+// let count=1
+
+// button.addEventListener('click',function(){
+
+// let liElement=document.createElement('li');
+
+
+
+// liElement.innerText=count;
+
+
+// // liElement.classList.add('background-color ')
+// ulElement.appendChild(liElement)
+
+// count++;
+
+// })
